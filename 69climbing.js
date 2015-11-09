84e47229987772cb9d6593e296cd2e73936364ad
Koji Horaguchi <koji.horaguchi@gmail.com>
2015-11-09 23:23:44 +0900

    Implemented the wall

--- 69climbing.js
+++ 69climbing.js
@@ -15,50 +15,18 @@ var SixtyNineClimbing = function () {
 // for node.js, not for CommonJS
 module.exports = SixtyNineClimbing;
 
+// ,;-+/([{&#
+//  perl -le '$i = 255; for (0 .. 7) { print sprintf("%x", $i - $_ * 20); }'
 SixtyNineClimbing.getStrFromValue = function (value) {
-  if (value > 0.9) {
-    return '9';
-  } else if (value > 0.8) {
-    return '8';
-  } else if (value > 0.7) {
-    return '7';
-  } else if (value > 0.6) {
-    return '6';
-  } else if (value > 0.5) {
-    return '5';
-  } else if (value > 0.4) {
-    return '4';
-  } else if (value > 0.3) {
-    return '3';
-  } else if (value > 0.2) {
-    return '2';
-  } else if (value > 0.1) {
-    return '1';
-  } else if (value > 0) {
-    return '0';
-  } else if (value > -0.1) {
-    return 'a';
-  } else if (value > -0.2) {
-    return 'b';
-  } else if (value > -0.3) {
-    return 'c';
-  } else if (value > -0.4) {
-    return 'd';
-  } else if (value > -0.5) {
-    return 'e';
-  } else if (value > -0.6) {
-    return 'f';
-  } else if (value > -0.7) {
-    return 'g';
-  } else if (value > -0.8) {
-    return 'h';
-  } else if (value > -0.9) {
-    return 'i';
-  } else if (value > -1) {
-    return 'j';
-  } else {
-    return '?';
-  }
+  if (value > 0.75)         { return '{#656565-fg}.{/#656565-fg}';
+  } else if (value > 0.5)   { return '{#7b7b7b-fg}:{/#7b7b7b-fg}';
+  } else if (value > 0.25)  { return '{#919191-fg}-{/#919191-fg}';
+  } else if (value > 0)     { return '{#a7a7a7-fg}={/#a7a7a7-fg}';
+  } else if (value > -0.25) { return '{#bdbdbd-fg}+{/#bdbdbd-fg}';
+  } else if (value > -0.5)  { return '{#d3d3d3-fg}*{/#d3d3d3-fg}';
+  } else if (value > -0.75) { return '{#e9e9e9-fg}#{/#e9e9e9-fg}';
+  } else if (value > -1)    { return '{#ffffff-fg}${/#ffffff-fg}';
+  } else { return '?'; }
 };
 
 SixtyNineClimbing.prototype.createScreen = function () {
@@ -69,7 +37,7 @@ SixtyNineClimbing.prototype.createScreen = function () {
   var getStrFromValue = SixtyNineClimbing.getStrFromValue;
   for (var y = 0; y < 48; ++y) {
     for (var x = 0; x < 54; ++x) {
-      screen[y][x] = getStrFromValue(noise.simplex2(x / 10, y / 10));
+      screen[y][x] = getStrFromValue(noise.simplex2(x / 20, y / 10));
     }
   }
 };
