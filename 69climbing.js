5e4936c1d14a9b4327534423cf86023bffda9dcd
Koji Horaguchi <koji.horaguchi@gmail.com>
2015-11-11 23:00:38 +0900

    Implemented levels

--- 69climbing.js
+++ 69climbing.js
@@ -11,8 +11,8 @@ var SixtyNineClimbing = function () {
     }
     screen.push(row);
   }
-  this.playerX = 100;
-  this.playerY = 100;
+  this.playerX = 0;
+  this.playerY = 0;
   this.updateScreen(this.playerX, this.playerY);
 };
 // for node.js, not for CommonJS
@@ -20,16 +20,16 @@ module.exports = SixtyNineClimbing;
 
 // . : - = + * # $
 // perl -le '$i = 255; for (0 .. 7) { print sprintf("%x", $i - $_ * 22); }'
-SixtyNineClimbing.getStrFromValue = function (value, player) {
-  if (player) {
-    if (value > 0.75)         { return '{#ff0000-fg}@{/#ff0000-fg}';
-    } else if (value > 0.5)   { return '{#ff0000-fg}@{/#ff0000-fg}';
-    } else if (value > 0.25)  { return '{#ff0000-fg}@{/#ff0000-fg}';
-    } else if (value > 0)     { return '{#ff0000-fg}@{/#ff0000-fg}';
-    } else if (value > -0.25) { return '{#ff0000-fg}@{/#ff0000-fg}';
-    } else if (value > -0.5)  { return '{#ff0000-fg}@{/#ff0000-fg}';
-    } else if (value > -0.75) { return '{#ff0000-fg}@{/#ff0000-fg}';
-    } else if (value > -1)    { return '{#ff0000-fg}@{/#ff0000-fg}';
+SixtyNineClimbing.getStrFromValue = function (value, rock) {
+  if (!rock) {
+    if (value > 0.75)         { return '{#00ff00-fg}"{/#00ff00-fg}';
+    } else if (value > 0.5)   { return '{#aaaaaa-fg}.{/#aaaaaa-fg}';
+    } else if (value > 0.25)  { return '{#aaaaaa-fg}.{/#aaaaaa-fg}';
+    } else if (value > 0)     { return '{#aaaaaa-fg}.{/#aaaaaa-fg}';
+    } else if (value > -0.25) { return '{#aaaaaa-fg}.{/#aaaaaa-fg}';
+    } else if (value > -0.5)  { return '{#aaaaaa-fg}.{/#aaaaaa-fg}';
+    } else if (value > -0.75) { return '{#aaaaaa-fg}.{/#aaaaaa-fg}';
+    } else if (value > -1)    { return '{#aaffaa-fg}"{/#aaffaa-fg}';
     } else { return '?'; }
   } else {
     if (value > 0.75)         { return '{#8c8c8c-fg}.{/#8c8c8c-fg}';
@@ -43,14 +43,65 @@ SixtyNineClimbing.getStrFromValue = function (value, player) {
     } else { return '?'; }
   }
 };
+SixtyNineClimbing.LEVEL = [
+  [    0, 0 ],
+  [   10, 1 ], //    6
+  [   16, 0 ],
+  [   66, 2 ], //   10
+  [   76, 0 ],
+  [  125, 3 ], //   25
+  [  150, 0 ],
+  [  200, 4 ], //   50
+  [  250, 0 ],
+  [  300, 5 ], //   75
+  [  375, 0 ],
+  [  425, 6 ], //  100
+  [  525, 0 ],
+  [  575, 7 ], //  200
+  [  775, 0 ],
+  [  850, 8 ], //  500
+  [ 1350, 0 ],
+  [ 1400, 9 ], // 1000
+  [ 2400, 0 ]
+];
+SixtyNineClimbing.isRockFromY_cache = {};
+SixtyNineClimbing.isRockFromY = function (y) {
+  if (y > 0) {
+    return 0;
+  }
+  var abs_y = Math.abs(y);
+  var cache = SixtyNineClimbing.isRockFromY_cache;
+  if (cache.hasOwnProperty(abs_y)) {
+    return cache[abs_y];
+  }
+  var level = SixtyNineClimbing.LEVEL;
+  var result = 0;
+  for (var i = 0, l = level.length; i < l; ++i) {
+    var value = level[i];
+    if (value[0] <= abs_y) {
+      result = value[1];
+    } else {
+      break;
+    }
+  }
+  cache[abs_y] = result;
+  return result;
+};
 
 SixtyNineClimbing.prototype.updateScreen = function (px, py) {
   var screen = this.screen;
   var noise = perlin.noise;
   var getStrFromValue = SixtyNineClimbing.getStrFromValue;
   for (var y = 0; y < 48; ++y) {
+    var is_rock = SixtyNineClimbing.isRockFromY(y + py);
     for (var x = 0; x < 54; ++x) {
-      screen[y][x] = getStrFromValue(noise.simplex2((x + px) / 16, (y + py) / 8), (x === 27 && y === 30));
+      if (x === 27 && y === 30) {
+        screen[y][x] = '{#ff0000-fg}@{/#ff0000-fg}';
+      } else if ((y + py) > 35) {
+        screen[y][x] = ' ';
+      } else {
+        screen[y][x] = getStrFromValue(noise.simplex2((x + px) / 20, (y + py) / 10), is_rock);
+      }
     }
   }
 };
